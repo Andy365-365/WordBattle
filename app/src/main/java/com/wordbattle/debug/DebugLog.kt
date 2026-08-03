4a2424ed730309599b4b7f28c3a1866bfa723185
Andy365 <andy365@nousresearch.com>
2026-08-03 20:18:58 +0800

    chore: bump version tag to v1.9-answer-listener-fix

--- app/src/main/java/com/wordbattle/debug/DebugLog.kt
+++ app/src/main/java/com/wordbattle/debug/DebugLog.kt
@@ -12,7 +12,7 @@ import java.util.Locale
 import java.util.concurrent.ConcurrentLinkedQueue
 
 object DebugLog {
-    const val VERSION = "v1.8-restart-fix"
+    const val VERSION = "v1.9-answer-listener-fix"
 
     private val queue = ConcurrentLinkedQueue<String>()
     private var logFile: File? = null
